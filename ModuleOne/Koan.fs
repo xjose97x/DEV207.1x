5c4d7b3e78825de51530eeaf335eb892a8928cc3
xjose97x <joseignacioescudero97@hotmail.com>
2015-10-18 21:52:50 -0500

    Koan

---
+++ ModuleOne/Koan.fs
@@ -0,0 +1,20 @@
+﻿(********************************************
+*DEV207.1x: Module One Assignment           *
+*Program Name: Koan	        *                    
+*Programming Language: F#                   *
+*Author: José I. Escudero                   *
+*E-mail: joseignacioescudero97@hotmail.com  *
+*Date(DD/MM/YYYY): 18/10/2015               *
+********************************************)
+open System
+
+let a = 100
+let b = 1000
+let f x y = x + y / 10
+[<EntryPoint>]
+let main argv =
+    let a = 101
+    let ans = f a b
+    printfn "%A" ans
+    Console.ReadKey()
+    0
