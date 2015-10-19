ea2bf0353755cd7a754ef039d5c67cc09c061c31
xjose97x <joseignacioescudero97@hotmail.com>
2015-10-18 22:15:21 -0500

    Module One Completed

--- ModuleOne/ModuleOne.fs
+++ ModuleOne/ModuleOne.fs
@@ -1,9 +1,23 @@
-﻿open Android.App
-open Android.Content
-open Android.OS
-open Android.Runtime
-open Android.Views
-open Android.Widget
+﻿(********************************************
+*DEV207.1x: Module One Assignment           *
+*Program Name: Cylinder Volume  	        *                    
+*Programming Language: F#                   *
+*Author: José I. Escudero                   *
+*E-mail: joseignacioescudero97@hotmail.com  *
+*Date(DD/MM/YYYY): 18/10/2015               *
+********************************************)
+open System
 
-module ModuleOne
-
+let CylinVol (r : float) (h : float) = Math.PI * Math.Pow(r, 2.0) * h
+[<EntryPoint>]
+let main argv =
+    let ask = Console.WriteLine("Radius of Cylinder?")
+    let r = Console.ReadLine() 
+    let r = float r
+    let ask1 = Console.WriteLine("Height of Cylinder?")
+    let h = Console.ReadLine()
+    let h = float h
+    let calc = CylinVol r h
+    Console.WriteLine("The Volume of the Cylinder with the data you provided is: {0}", calc)
+    Console.ReadKey();
+    0
