4f30339ccf8b17e4148729afe3497f7d5f1270db
xjose97x <joseignacioescudero97@hotmail.com>
2015-10-22 21:04:55 -0500

    Initial Commit

---
+++ ModuleTwo Koan/ModuleTwo Koan.fs
@@ -0,0 +1,19 @@
+﻿(********************************************
+*DEV207.1x                                  *
+*Program Name: Module Two Koan              *                    
+*Programming Language: F#                   *
+*Author: José I. Escudero                   *
+*E-mail: joseignacioescudero97@hotmail.com  *
+********************************************)
+
+open System
+
+let rec dosomethingrandom x =
+    if x = 0 then Console.WriteLine(1)
+    else dosomethingrandom ((x - 1) * x)
+[<EntryPoint>]
+let main argv = 
+    let input = Console.ReadLine()
+    let input = int input
+    let calc = dosomethingrandom input
+    0
